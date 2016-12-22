9a65405504405d2e464ae237d85ab60832deb39d
Alexandre DE PEMBROKE <Alexandre.DEPEMBROKE@Digital-Grenoble.local>
2016-12-22 10:10:32 +0100

    create template admin

---
+++ resources/views/layout/default_admin.blade.php
@@ -0,0 +1,19 @@
+@extends('layout.default_co')
+
+@section('content')
+	<div class="row">
+		<div class="col-lg-offset-3 col-lg-7 col-md-8 col-sm-12 col-xs-12">
+			<div class="bloc_fil_actu">
+    			<h1>Admin Page</h1>
+    			<ul class="nav nav-tabs">
+					<li role="presentation" class=" @yield('adminTagsClass') "><a href="/admin/tags">Historique des Tags</a></li>
+					<li role="presentation" class=" @yield('adminPostsClass') "><a href="/admin/post">Posts ayant été signalés</a></li>
+					<li role="presentation" class=" @yield('adminEventsClass') "><a href="/admin/events">Historique des événements</a></li>
+				</ul>
+
+				@yield('admin_content')
+				
+			</div>
+		</div>
+	</div>
+@endsection
