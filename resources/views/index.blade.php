9d4b628d16c7c6e4ad6765d90961a31781ef233f
Julie DETTOMA <Julie.DETTOMA@Digital-Grenoble.local>
2016-12-22 14:54:57 +0100

    modif btn connexion & inscription on index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,8 +4,8 @@
 
 @section("btn_login")
 	<div class="col-lg-2 col-md-2 col-xs-2 vcenter">
-		<a href="/connexion" class="btn btn-default btn-logout notif" role="button"> Connecte-toi </a>
-		<a href="/inscription" class="btn btn-default btn-logout notif" role="button"> Inscription </a>
+		<a href="/connexion" class="btn btn-index btn-default btn-logout notif" role="button"> Connecte-toi </a>
+		<a href="/inscription" class="btn btn-index btn-default btn-logout notif" role="button"> Inscription </a>
 	</div>
 @endsection
 
