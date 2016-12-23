41a084ca40ce0dd1f91f8bccda800a57a66a9be2
Julie DETTOMA <Julie.DETTOMA@Digital-Grenoble.local>
2016-12-23 14:49:52 +0100

    modif styles btn connect, font

--- resources/views/login/connexion.blade.php
+++ resources/views/login/connexion.blade.php
@@ -7,18 +7,18 @@
 	   	<div class="row">
 	   		<div class="col-lg-offset-2 col-md-offset-2 col-lg-7 col-md-7 col-sm-12 col-xs-12">
 		   		<div class=" bloc_fil_actu">
-				   	<div class="col-lg-offset-4 col-lg-4 form_connexion">
+				   	<div class="col-lg-offset-4 col-lg-8 col-md-8 col-sm-8 col-xs-8 form_connexion text-center">
 					   	<fieldset>
-					   		<h2>Connexion</h2>
-						   	<form action="http://campus-numerique-in-the-alps.com/formulaire/index.php"  method="post"  onsubmit="return verifFormConnexion(this)">
+					   		<h2>Connecte-toi</h2>
+						   	<form class="text-center" role="form" action="http://campus-numerique-in-the-alps.com/formulaire/index.php"  method="post"  onsubmit="return verifFormConnexion(this)">
 								<div class="form-group">
-								 	<label for="pseudo_connexion">Pseudo:</label>
+								 	<label for="pseudo_connexion" class="col-sm-2 control-label""></label>
 								    <input type="text" class="form-control" name="pseudo_connexion"  placeholder="Pseudo" id="pseudo_connexion" required  onBlur="verifPseudo(this)">
 								    <div class="error" id="alert_pseudo_connexion"></div>
 								 </div>
 								 <div class="form-group">
-								    <label for="pwd_connexion">Mot de passe:</label>
-								    <input type="password" class="form-control" name="pwd_connexion"  placeholder="mot de passe" id="pwd_connexion" required onBlur="verifPassword(this)">
+								    <label for="pwd_connexion" class="col-sm-2 control-label""></label>
+								    <input type="password" class="form-control" name="pwd_connexion"  placeholder="Mot de passe" id="pwd_connexion" required onBlur="verifPassword(this)">
 								    <div class="error" id="alert_password"></div>
 								 </div>
 								 <div class="checkbox">
@@ -26,17 +26,20 @@
 								    	<input type="checkbox" name="box" value="rememberMe">Se souvenir de moi
 								    </label>
 								 </div>
-								  	<input type="submit" class="btn btn-default" value="Se Connecter" >
+								
+								  	<input type="submit" class="btn btn-lg btn-default" value="Se Connecter" >
+								
 							</form>
 						</fieldset>
 							<a href="/reinitialisation">Réinitialiser votre mot de passe</a>
 					</div>
 					<div class="row">
-						<div class="col-lg-offset-4 col-lg-6 col-md-6 col-sm-6 form_connexion">
-							<h2>Connexion via les Réseaux Sociaux</h2>					
-							<button class="btn btn-sm btn-default">Connexion <i class="fa fa-twitter-square"></i></button>
-							<button class="btn btn-sm btn-primary">Connexion <i class="fa fa-facebook-square"></i></button>
-							<button class="btn btn-sm btn-danger">Connexion <i class="fa fa fa-google-plus-square"></i></button>
+						<div class="col-lg-offset-4 col-lg-8 col-md-8 col-sm-8 col-xs-8 form_connexion text-center">
+							<h2>Connecte-toi via </h2>	
+								<button class="btn btn-lg btn-default"><i class="fa fa-twitter-square"></i></button>
+								<button class="btn btn-lg btn-default"><i class="fa fa-facebook-square"></i></button>
+								<button class="btn btn-lg btn-default"><i class="fa fa fa-google-plus-square"></i></button>
+		
 						</div>
 					</div>
 				</div>
