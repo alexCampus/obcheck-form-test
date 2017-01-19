f6804ed4477291abf7557dddb4431e6c666c7755
Julie DETTOMA <Julie.DETTOMA@Digital-Grenoble.local>
2017-01-19 10:31:39 +0100

    update form events page

--- resources/views/user/event/create_event.blade.php
+++ resources/views/user/event/create_event.blade.php
@@ -14,63 +14,63 @@
 	   	    
 	<div class="col-lg-9 col-md-8 col-sm-12 col-xs-12">
 		<div class="bloc_fil_actu">				
-			<h2>créer mon événement</h2>
-			<div class="col-lg-7 col-md-7 col-sm-7">
-				<form action="http://campus-numerique-in-the-alps.com/formulaire/index.php" method="post" onsubmit="return verifFormCreateEvent(this)">
-					<div class="form-group">
-						<label for="intitulé">Intitulé
-							<input type="text" name="intitule" class="form-control"  placeholder="Intitulé" required id="intitule" onblur="verifLettre(this)">
-							<div class="error" id="alert_intitule"></div>
-						</label>
-					</div>
-					<div class="form-group">
-						<label for="description">description
-							<textarea class="form-control" name="description" placeholder="description" required id="description" onblur="verifPublication(this)"></textarea>
-							<div class="error" id="alert_description"></div>
-						</label>
-					</div>
-					<div class="form-group">
-						<label for="date">Date
-							<input type="date" name="date" class="form-control" required id="date" onblur="verifDate(this)">
-							<div class="error" id="alert_date"></div>
-						</label>
-					</div>
-					<div class="form-group">
-						<label for="lieu">Lieu
-							<input type="text" name="lieu" class="form-control"  placeholder="lieu" required id="lieu" onblur="verifLettre(this)">
-							<div class="error" id="alert_lieu"></div>
-						</label>
-					</div>
+			<h2>Je crée mon événement</h2>
 
-					<div class="form-group">
-						<label for="categorie">Catégorie
-							<select class="form-control" id="categorie" name="categorie" required>
-								 	<option disabled selected value="0"> selectionner une option</option>
-								 	<option value="1">Animaux</option>
-								 	<option value="2">Gastronomie</option>
-								 	<option value="3">Loisirs Créatifs</option>
-								 	<option value="4">Maison</option>
-								 	<option value="5">Multimédia</option>
-								 	<option value="6">Musique</option>
-								 	<option value="7">Nature</option>
-								 	<option value="8">Sports</option>
-								 	<option value="9">Véhicules</option>
-								  	<option value="10">Voyages</option>									  	
-							</select>
-						</label>
-					</div>
-					<div class="form-group">
-                    	<label for="tag">tag
-                    		<input type="text" name="tag" class="form-control"  placeholder="#hashtags" required id="tag" onblur="verifTag(this)">
-                    		<div class="error" id="alert_tag"></div>
-                    	</label>
-     				</div>							
-					<input type="submit" class="btn btn-default">
-				</form>
-			</div>
-			<div class="clear"></div>
+			<form action="" method="post" onsubmit="return verifFormCreateEvent(this)">
+  				<div class="form-group">
+    				<label for="intitulé">Intitulé</label>
+					<input type="text" name="intitule" class="form-control"  placeholder="Intitulé" required id="intitule" onblur="verifLettre(this)">
+							<div class="error" id="alert_intitule"></div>
+  				</div>
+  				<div class="form-group">
+    				<label for="description">description</label>
+    				<textarea class="form-control" name="description" placeholder="description" required id="description" onblur="verifPublication(this)"></textarea>
+					<div class="error" id="alert_description"></div>
+  				</div>
+  				
+  				<div class="form-group">
+					<label for="date">Date</label>
+					<input type="date" name="date" class="form-control" required id="date" onblur="verifDate(this)">
+					<div class="error" id="alert_date"></div>
+				</div>
+  				
+  				<div class="form-group">
+					<label for="lieu">Lieu</label>
+					<input type="text" name="lieu" class="form-control"  placeholder="lieu" required id="lieu" onblur="verifLettre(this)">
+					<div class="error" id="alert_lieu"></div>	
+				</div>
+
+				<div class="form-group">
+					<label for="categorie">Catégorie</label>
+					<select class="form-control" id="categorie" name="categorie" required>
+						<option disabled selected value="0"> selectionner une option</option>
+						<option value="1">Animaux</option>
+						<option value="2">Gastronomie</option>
+						<option value="3">Loisirs Créatifs</option>
+						<option value="4">Maison</option>
+						<option value="5">Multimédia</option>
+						<option value="6">Musique</option>
+						<option value="7">Nature</option>
+						<option value="8">Sports</option>
+						<option value="9">Véhicules</option>
+						<option value="10">Voyages</option>									  	
+					</select>
+				</div>
+
+				<div class="form-group">
+                    <label for="tag">tag</label>
+                    <input type="text" name="tag" class="form-control"  placeholder="#hashtags" required id="tag" onblur="verifTag(this)">
+                    <div class="error" id="alert_tag"></div>
+     			</div>	
+
+  				<button type="submit" class="btn btn-default">Valider</button>
+			</form>
+
 		</div>
-	</div>				
+
+			<div class="clear"></div>
+	</div>
+					
 		
 @endsection		
 	
